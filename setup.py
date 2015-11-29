2ee7a2fd85448fd4fdac96c17283646addaa1400
Arnold Bechtoldt <mail@arnoldbechtoldt.com>
2015-11-29 09:30:31 +0100

    add missing setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(name='project_sync',
+      version='0.0.1',
+      description='KISS-styled project file maintenance using metadata',
+      author='Arnold Bechtoldt',
+      author_email='mail@arnoldbechtoldt.com',
+      url='https://github.com/bechtoldt/project-sync',
+      packages=['project_sync'], )
